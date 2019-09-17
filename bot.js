d5dcc3103047e8e93d4faa1991782dc76146315d
NichyX <nichy2010@hotmail.it>
2019-09-17 23:05:51 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -20,6 +20,30 @@ client.on('message', message => {
   	}
 });
 
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  // Here I'm storing the IDs of their voice channels, if available
+  let oldChannel = oldMember.voiceChannel ? oldMember.voiceChannel.id : null;
+  let newChannel = newMember.voiceChannel ? newMember.voiceChannel.id : null;
+  if (oldChannel == newChannel) return; // If there has been no change, exit
+
+  // Here I'm getting the bot's channel (bot.voiceChannel does not exist)
+  let botMember = oldMember.guild.member(bot.user),
+    botChannel = botMember ? botMember.voiceChannel.id : null;
+
+  // Here I'm getting the channel, just replace VVV this VVV with the channel's ID
+  let textChannel = oldMember.guild.channels.get('420341102432747520');
+  if (!textChannel) throw new Error("That channel does not exist.");
+
+  // Here I don't need to check if they're the same, since it would've exit before
+  if (newChannel == botChannel) {
+    // console.log("A user joined.");
+    textChannel.send(`${newMember} has joined the voice channel.`);
+  } else if (oldChannel == botChannel) {
+    // console.log("A user left.");
+    textChannel.send(`${newMember} has left the voice channel.`);
+  }
+});
+
 // Create an event listener for messages
 client.on('message', message => {
   // If the message is "what is my avatar"
