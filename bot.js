c9fbb51c3e2b46b5b4df5207a55845c46c4f21b1
NichyX <nichy2010@hotmail.it>
2019-09-17 23:20:25 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,11 +1,19 @@
 // Extract the required classes from the discord.js module
 const { Client, RichEmbed } = require('discord.js');
+const fs = require("fs");
+
+let channels;
+let data = null;
+let voiceLogChannel = "";
+let prefix = "";
 
 // Create an instance of a Discord client
 const client = new Client();
 
 client.on('ready', () => {
     console.log('Sono vivo!');
+	updateChannels();
+	setLogChannel(data.voiceLogChannel);
 });
 
 client.on('message', message => {
@@ -21,28 +29,99 @@ client.on('message', message => {
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-  // Here I'm storing the IDs of their voice channels, if available
-  let oldChannel = oldMember.voiceChannel ? oldMember.voiceChannel.id : null;
-  let newChannel = newMember.voiceChannel ? newMember.voiceChannel.id : null;
-  if (oldChannel == newChannel) return; // If there has been no change, exit
+	// Check if voiceLogChannel has been set
+	if (data.voiceLogChannel === "")
+		return;
 
-  // Here I'm getting the bot's channel (bot.voiceChannel does not exist)
-  let botMember = oldMember.guild.member(client.user),
-    botChannel = botMember ? botMember.voiceChannel.id : null;
+	let username = oldMember.displayName;
+	let oldVCID = oldMember.voiceChannelID;
+	let newVCID = newMember.voiceChannelID;
 
-  // Here I'm getting the channel, just replace VVV this VVV with the channel's ID
-  let textChannel = oldMember.guild.channels.get('420341102432747520');
-  if (!textChannel) throw new Error("That channel does not exist.");
-
-  // Here I don't need to check if they're the same, since it would've exit before
-  if (newChannel == botChannel) {
-    // console.log("A user joined.");
-    textChannel.send(`${newMember} has joined the voice channel.`);
-  } else if (oldChannel == botChannel) {
-    // console.log("A user left.");
-    textChannel.send(`${newMember} has left the voice channel.`);
-  }
+	let oldChannelName = (oldVCID != null && typeof oldVCID != undefined) ? channels.get(oldVCID).name : null;
+	let newChannelName = (newVCID != null && typeof newVCID != undefined) ? channels.get(newVCID).name : null;
+	
+	if (oldChannelName === null)
+		voiceLogChannel.sendMessage(`${username} connected to voice and joined ${newChannelName}`);
+	else if (newChannelName === null)
+		voiceLogChannel.sendMessage(`${username} disconnected`);
+	else
+		voiceLogChannel.sendMessage(`${username} moved to ${newChannelName}`);
 });
+
+client.on('channelCreate', (channel) => {
+	updateChannels();
+});
+
+client.on('channelDelete', (channel) => {
+	updateChannels();
+});
+
+
+client.on('message', (message) => {
+	let msg = message.content;
+
+	if (!msg.startsWith(prefix))
+		return;
+
+	// extract command and parameters
+	let cmd = msg.replace(prefix, '').slice(0, msg.indexOf(' ') - 1);
+	let params = msg.slice(msg.indexOf(' ') + 1, msg.length + 1);
+
+	// check if the user has permission to make changes to the bot
+	if (!message.member.hasPermission("ADMINISTRATOR", true))
+		return;
+	else {
+		switch(cmd) {
+			case "setLogChannel":
+				// Primitive implementation, pls change
+				setLogChannel(params, message.channel);
+				writedata();
+				break;
+			default:
+				message.channel.sendMessage("Sorry, I don't recognize that command T_T.");
+		}
+	}
+});
+
+let readdata = function () {
+	data = JSON.parse(fs.readFileSync("data.json"));
+	token = data.token;
+	prefix = data.prefix;
+}
+
+let writedata = function () {
+	fs.writeFile('data.json', JSON.stringify(data), 'utf8', () => {
+		console.log('Data written successfully!');
+	});
+}
+
+let updateChannels = function () {
+	channels = client.channels;
+}
+
+
+let setLogChannel = function (channel, msgChannel) {
+	voiceLogChannel = channels.get(channels.findKey('name', channel));
+
+	// Return if no channel has been set
+	if (typeof voiceLogChannel === 'undefined')
+		return;
+	else if (typeof voiceLogChannel === 'undefined' && msgChannel) {
+		msgChannel.sendMessage(`Couldn't find channel '${channel}'`);
+		return;
+	}
+	else if (voiceLogChannel.type === 'voice' && msgChannel) {
+		msgChannel.sendMessage(`Can only log to text channels`);
+		return;
+	}
+
+	data.voiceLogChannel = channel;
+
+	if (msgChannel)
+		msgChannel.sendMessage(`Channel for logging set to '${channel}'`);
+}
+
+readdata();
 
 // Create an event listener for messages
 client.on('message', message => {
